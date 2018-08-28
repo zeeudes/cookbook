1901872692c1d989eec2bfe97d6e924debc58f94
José Eudes de Souza Júnior <eudes.souza@ufpe.br>
2018-08-27 21:59:21 -0300

    Inserindo a receita
    classe de domínio criada e componente de lista de receitas conceitual.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../domain/Recipe.domain';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://imagesvc.timeincapp.com/v3/mm/' +
+    'image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_' +
+    'horizontal_inbody_900x506%2Fpublic%2F1502824044%2Froyal-farms-best-gas-station-food-FT-SS0817' +
+    '.jpg%3Fitok%3Dig79fdSU&w=700&q=85')
+  ];
 
   constructor() { }
 
